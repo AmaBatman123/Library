f72dda4d063a856dc52e8a4d660f00b059f66f90
Valerii <amabatman.12@gmail.com>
2024-10-21 18:09:16 +0600

    Add class Library

--- main.py
+++ main.py
@@ -14,3 +14,26 @@ class Book:
     def __str__(self):
         status = "Прочитана" if self.read else "Не прочитана"
         return f"{self.title} by {self.author} ({self.year}) - {status}"
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+        print(f"Книга '{book.title}' добавлена в библиотеку.")
+
+    def list_books(self):
+        if not self.books:
+            print("Библиотека пуста.")
+        else:
+            for book in self.books:
+                print(book)
+
+    def find_by_title(self, title):
+        found_books = [book for book in self.books if book.title.lower() == title.lower()]
+        if found_books:
+            for book in found_books:
+                print(book)
+        else:
+            print(f"Книга с названием '{title}' не найдена.")
